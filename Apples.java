d976250be6ada91eae4869c7a913c73e2204ef87
AnbhuSavi <saviprabhu123@gmail.com>
2017-08-14 14:23:21 +0530

    Create Apples.java

---
+++ Apples.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.io.*;
+import java.lang.*;
+
+public class Apples {
+
+    public static void main(String[] args, int i) { 
+ String s="hello";
+for( i=0;i<=s.length();i++)
+{
+    char e=s.charAt(i);
+    if((e== 'A')||(e=='E')||(e=='I')||(e=='O')||(e=='U')){
+        {
+String h=s.substring(0, i);
+
+String g=s.substring(i+ i);
+s=h+"Z"+g;
+}
+}
+}
+    System.out.println("The result is="+s);
+}
+}
